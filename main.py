1d04a984957cb745b93ea25ceea5f90b94be356f
Sashavav <vav110011@gmail.com>
2024-02-04 01:45:16 +0300

    Added all required constructions and learned to solve x AND y task

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import subprocess
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+"""
+config.txt structure:
+0 line: number of neurons on every layer
+1 line: activate function (1 - sigmoid; 2 - modRELu; 3 - th)
+2 line: using mode (0 - using from console; 1 - using from file; 2 - learning from datafile)
+"""
+file = open("config.txt")
+readfile = file.readlines()
+file.close()
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+cfg = readfile[0].split()
+cfg.append(readfile[1])  # adding function number in the end
+match readfile[2]:
+    case "0":
+        inputstring = input()  # working with 1d arrays rn
+        cfg.append(inputstring)
+        process = subprocess.run(['python', 'only Run network.py', *cfg])
+    case "1":
+        inputfile = open("input.txt")
+        cfg.append(inputfile.readlines()[0])
+        process = subprocess.run(['python', 'only Run network.py', *cfg])
+    case "2":
+        process = subprocess.run(['python', 'Learning.py', *cfg])
+"""output = process.stdout.strip()
+print(output)"""
---
+++ only Run network.py
@@ -0,0 +1,98 @@
+import math
+import sys
+import numpy
+
+
+class Function:
+    def __init__(self, choose):
+        self.choose = choose
+
+    def f(self, x):
+        match self.choose:
+            case 1:
+                return 1 / (1 + math.exp(-x))
+            case 2:
+                if x < 0:
+                    return x / 100
+                if x > 1:
+                    return x / 100
+                return x
+            case 3:
+                if x < 0:
+                    return (math.exp(x) - math.exp(-x)) / (100 * (math.exp(x) + math.exp(-x)))
+
+    def f_for_vector_arrays(self, array):
+        new_array = [0] * len(array)
+        for i in range(len(array)):
+            new_array[i] = Function.f(self, array[i])
+        return new_array
+
+
+class Matrix:
+    def __init__(self, rows, cols, matrix):
+        self.rows = rows
+        self.cols = cols
+        self.matrix = matrix
+
+    def multiplication_with_vector(self, rows, array):
+        if rows != self.cols:
+            exit("rows != cols in multiplication")
+        result = [0] * self.rows
+        for i in range(self.rows):
+            for j in range(self.cols):
+                result[i] += array[j] * self.matrix[i][j]
+        return result
+
+    def sum_vector_arrays(self, a, b):
+        if len(a)!=len(b):
+            exit('err in sum vector arrays')
+        c=[]
+        for i in range(len(b)):
+            c.append(a[i]+b[i])
+        return c
+
+
+class Network:
+    def __init__(self, size, choose, inputs, func):
+        self.network = []
+        self.network.append(inputs)
+        self.func = func
+        weights_file = open("weights" + choose + ".txt")
+        weights_file_string_array = weights_file.readlines()
+        weights_file_array = []
+        for i in range(len(weights_file_string_array)):
+            weights_file_array.append(list(map(float, weights_file_string_array[i].split())))
+        weights = []
+        bios = []
+        current_index = 0  # That's because final matrix is 3d, but file is 2d, so I need to convert 3d to 2d by
+        # adding special index
+        for i in range(len(size) - 1):
+            bios.append(numpy.zeros((size[i + 1])))
+            weights.append(numpy.zeros((size[i + 1], size[i])))
+            for j in range(size[i + 1]):
+                bios[i][j] = weights_file_array[current_index + j][0]
+                for k in range(size[i]):
+                    weights[i][j][k] = weights_file_array[current_index + j][k + 1]
+            current_index += size[i + 1]
+        self.weights = weights
+        self.bios = bios
+        weights_file.close()
+
+    def forward_feed(self):
+        for i in range(len(size) - 1):
+            a = Matrix
+            Matrix.__init__(a, size[i + 1], size[i], self.weights[i])
+            b = self.network[i]
+            self.network.append(Function.f_for_vector_arrays(self.func, Matrix.sum_vector_arrays(Matrix,Matrix.multiplication_with_vector(a, len(b), b), self.bios[i])))
+
+
+cfg = sys.argv[1:]
+inputs = list(map(float, cfg[-1].split()))
+func = Function
+Function.__init__(func, int(cfg[-2]))
+
+size = list(map(int, cfg[:-2]))
+Net = Network
+Network.__init__(Net, size, cfg[-2][:1], inputs, func)
+Network.forward_feed(Net)
+print(Net.network[-1])
